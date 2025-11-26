c202f852cea2325794d2854dde4ab331fefce152
Reynadi Andreas Priesley <rey@reynadi.com>
2025-11-26 10:42:23 +0700

    feat: adding search box

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { SidebarProvider, SidebarTrigger } from "./ui/sidebar";
 import { AppSidebar } from "./AppSidebar";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
@@ -8,7 +10,10 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <SidebarProvider>
         <AppSidebar />
         <main className="bg-purple-brand w-full h-screen flex flex-col overflow-hidden">
-          <SidebarTrigger className="text-white hover:bg-purple-brand-f hover:text-white size-10 m-4" />
+          <div className="flex flex-row">
+            <SidebarTrigger className="justify-self-start text-white hover:bg-purple-brand-f hover:text-white size-10 m-4" />
+            <Input className="max-w-md my-4 flex-1 text-white rounded-2xl bg-white/5 placeholder:text-white" placeholder="Search..." />
+          </div>
           <div className="flex-1 overflow-hidden px-4 pb-4">{children}</div>
         </main>
       </SidebarProvider>
